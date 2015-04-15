1f34d3d60f38bf56e175c7f3ef22a26f19af2a28
Luc Maignan <luc.maignan@gmail.com>
2015-04-15 19:29:37 +0200

    ping

---
+++ python/ping.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8
+
+__author__ = 'Luc Maignan'
+
+import subprocess
+import os
+import re
+
+#os.system("dir/w")
+
+host = 'free.fr'
+#subprocess.call(['ping', '-n', '2', host])
+pr = subprocess.Popen(['ping', '-n', '2', host], stdout=subprocess.PIPE)
+
+output = pr.communicate()[0]
+
+try:
+
+    perte = re.search(r"perte (\d*)", output).groups()
+    print "% de perte de paquets = " + perte[0]
+    temps = re.search(r"Minimum = (\d*).*Maximum = (\d*)", output).groups()
+    print "Temps minimal = {0[0]} ms, Temps maximal = {0[1]} ms".format(temps)
+    ip = re.search(r"pour (?P<ip>.*):", output).group('ip')
+    print 'IP pingée = ' + ip
+
+except AttributeError:
+    print "La requête n'a rien retourné !"
