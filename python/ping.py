231ed92b6b4752cb18c74d6cf0106263ef38a183
Luc Maignan <luc.maignan@gmail.com>
2015-04-29 19:10:57 +0200

    Correction bug pour la cible posix

--- python/ping.py
+++ python/ping.py
@@ -20,12 +20,22 @@ output = pr.communicate()[0]
 
 try:
 
-    perte = re.search(r"perte (\d*)", output).groups()
-    print "% de perte de paquets = " + perte[0]
-    temps = re.search(r"Minimum = (\d*).*Maximum = (\d*)", output).groups()
-    print "Temps minimal = {0[0]} ms, Temps maximal = {0[1]} ms".format(temps)
-    ip = re.search(r"pour (?P<ip>.*):", output).group('ip')
-    print 'IP pingée = ' + ip
+    if os.name == 'posix':
+
+        perte = re.search(r"(\d*)% packet loss", output).groups()
+        print "% de perte de paquets = " + perte[0]
+        temps = re.search(r"round-trip.* = (\d*\.\d*)/.*/(\d*\.\d*)/", output).groups()
+        print "Temps minimal = {0[0]} ms, Temps maximal = {0[1]} ms".format(temps)
+        ip = re.search(r"PING .* \((?P<ip>(\d*\.){3}\d*)", output).group('ip')
+        print 'IP pingée = ' + ip
+
+    else:
+        perte = re.search(r"perte (\d*)", output).groups()
+        print "% de perte de paquets = " + perte[0]
+        temps = re.search(r"Minimum = (\d*).*Maximum = (\d*)", output).groups()
+        print "Temps minimal = {0[0]} ms, Temps maximal = {0[1]} ms".format(temps)
+        ip = re.search(r"pour (?P<ip>.*):", output).group('ip')
+        print 'IP pingée = ' + ip
 
 except AttributeError:
     print "La requête n'a rien retourné !"
